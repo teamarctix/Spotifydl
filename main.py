ce534e048ae297054e9bfd9ba07bc82f713df898
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-02-10 08:13:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,174 @@
+from pyrogram import Client, filters
+import requests
+from sub import *
+import os
+from time import time
+import time
+from datetime import datetime
+from pytz import timezone
+
+from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup,
+                            InlineKeyboardButton)
+
+
+api_id = 3702208
+api_hash = "3ee1acb7c7622166cf06bb38a19698a9"
+bot_token = "5300188722:AAFlruACp00Hv2ZD1RPjE9P0FahI52swqpU"
+
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
+
+
+
+@app.on_message(filters.command("dl"))
+async def start_command(client,message):
+    chat_id = message.chat.id
+    link = message.text
+    if link[4:].startswith("https") or link.startswith("http"):
+          if link.split( "/")[4] == "track" :
+  
+             ytlink = ytsq(link[4:])
+             ytdl(ytlink[1])
+      
+             for name in os.listdir():
+               if ytlink[0] in name: 
+                await app.send_audio(chat_id, audio=name,caption=name)
+                write(link[4:],name)
+                os.system("rm '"+name+"'")
+
+          else:
+                 info =  dytdl(link[4:])
+                 for name in os.listdir():
+                     if info[0] in name:
+                      await app.send_document(chat_id, document=name,caption=name )
+                      write(link[4:],name)
+                      os.system("rm '"+name+"'")
+             
+    else:
+           await app.send_message(channel_id,"Link Not Vaild!!!!")
+
+
+
+
+
+
+
+
+@app.on_message(filters.command("update"))
+async def start_command(client,message):
+         cdid= message.chat.id
+         button_list =[]
+         for each in pread():
+             button_list.append([InlineKeyboardButton(each[1], callback_data =str(each[2]))]) 
+             reply_markup=InlineKeyboardMarkup(button_list)
+         await app.send_message(
+            cdid,"Select The Required Playlist:",reply_markup=reply_markup)
+
+
+
+
+
+@app.on_callback_query()
+async def answer(client, call):
+          msgid = call.message.id
+          chat = call.message.chat.id
+          print(chat,msgid)
+          await app.edit_message_text(chat,msgid,"Update Started!!!")
+          playlist_id = call.data
+          URL2 = "https://api.spotify.com/v1/playlists/"+ playlist_id +"?access_token=" + token 
+          r2 = requests.get(URL2)
+          img = r2.json()['images'][0]['url']
+          df = r2.json()['name']
+          total = r2.json()['tracks']['total']
+          now=datetime.now()
+          crtda = now.strftime('%m/%d %I:%M:%S %p')
+          if os.getcwd().endswith("Spotdl"):
+             os.system("mkdir " + df)
+          else:
+             os.chdir("../")
+             os.system("mkdir " + df)
+          stats =  f'<b>├  Playlist Name: </b>{df}\n'\
+                        f'<b>├  Total No Of Songs: </b>{total}\n'\
+                        f'<b>╰ Updated Time: </b>{crtda}\n\n'
+          os.chdir(df)
+          #os.system("spotdl "+playlistlink)
+          await app.send_photo(chat,photo=img,caption=stats)
+          for filename in os.listdir():
+              if filename.endswith(".mp3"):
+                print(filename)
+                await app.send_audio(chat, audio=name,caption=name)
+
+
+
+
+@app.on_message(filters.command("updateall"))
+async def start_command(client,message):
+             channel_id = message.chat.id
+             #await app.send_message(channel_id,"Playlist!")
+             for id in pread():
+               playlist_id = id[0][34:].split('?')[0]
+               URL2 = "https://api.spotify.com/v1/playlists/"+ playlist_id +"?access_token=" + token 
+               r2 = requests.get(URL2)
+               img = r2.json()['images'][0]['url']
+               df = r2.json()['name']
+               total = r2.json()['tracks']['total']
+               now=datetime.now()
+               crtda = now.strftime('%m/%d %I:%M:%S %p')
+               # print(os.getcwd())
+               if os.getcwd().endswith("Spotdl"):
+                    os.system("mkdir " + df)
+               else:
+                    os.chdir("../")
+                    os.system("mkdir " + df)
+               stats = f'<b>├  Playlist Name: </b>{df}\n'\
+                           f'<b>├  Total No Of Songs: </b>{total}\n'\
+                           f'<b>╰ Updated Time: </b>{crtda}\n\n'
+               os.chdir(df)
+               #os.system("spotdl "+id[0])
+               await app.send_photo(channel_id,photo=img,caption=stats)
+               for filename in os.listdir():
+                  if filename.endswith(".mp3"):
+                   print(filename)
+                  #await app.send_audio(channel_id, audio=name,caption=name)
+
+
+
+
+
+
+
+
+
+
+@app.on_message(filters.command("add"))
+async def start_command(client,message):
+    link = message.text[5:]
+    pwrite(link)
+    await message.reply("Added Playlist!")
+    
+
+
+
+@app.on_message(filters.command("list"))
+async def start_command(client,message):
+         cdid= message.chat.id
+         pylnames =[]
+         for  pyname in pread():
+             pylnames.append([InlineKeyboardButton(pyname[1], callback_data =str(pyname[2]))]) 
+             reply_markup=InlineKeyboardMarkup(pylnames)
+         list = await app.send_message(
+            cdid,"Playlists:",reply_markup=reply_markup)
+         time.sleep(5)
+         msgid =list.id
+         chat = list.chat.id
+         await app.delete_messages(chat,msgid)
+       
+
+
+
+
+
+app.run()  # Automatically start() and idle()
---
+++ sub.py
@@ -0,0 +1,171 @@
+from youtubesearchpython import *
+import yt_dlp
+import json,csv,os
+import request
+
+
+
+
+
+code = AQA3nuVGFliJ9tzf22UK4xDJZFIJE8x69BRSQNPOY5j4Dftre_YQg4X7uI0eoCoJtiKrLGUoXXJ99msYdqnutybrZ4eC9MmA7VoV0KWR5PTuucYOTQOQdgreGS6ZhprgNwg
+
+
+
+token_data = {
+    "grant_type": "refresh_token",
+    "refresh_token": code,
+    "redirect_uri": "http://localhost:7777/callback"
+}
+
+
+def retk():
+     r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
+     token = r.json()["access_token"]
+     return token
+
+
+
+token=retk()
+
+
+def plnm(playlistlink):
+           playlist_id = playlistlink[34:].split('?')[0]
+           URL = "https://api.spotify.com/v1/playlists/" + playlist_id  + "/tracks?access_token=" + token
+           r = requests.get(url = URL) 
+           items= r.json()['items']
+           return items
+
+
+def gettrack(track):
+     global token
+     URL = "https://api.spotify.com/v1/tracks/" + track + "?access_token=" + token
+     r = requests.get(url = URL) 
+     data = r.json()
+     track = data['name']
+     sample= data['preview_url']
+     img = data['album']['images'][0]['url']
+     return track, sample, img 
+
+
+
+def rclone(name,name2):
+    rcl = "rclone --config './rclone.conf' copy '" +name+ "' 'Mirror:/Bot/"+ name2 +"'"
+    print(rcl)
+    os.system(rcl)
+
+
+
+
+def write(link,name):
+  with open("links.csv","a+") as filec:
+           cwrite = csv.writer(filec)
+           cwrite.writerow([link,name])
+
+
+
+def read():
+   global cread
+   filec = open("links.csv","r")
+   cread=csv.reader(filec)
+   return cread
+ 
+def ytsq(link):
+   tkid=link.split("/")[4].split("?")[0]
+
+   videosSearch = VideosSearch(gettrack(tkid)[0], limit = 1)
+   #print(gettrack(tkid)[0])
+   #print(videosSearch.result()['result'][0]['link'])
+   id = videosSearch.result()['result'][0]['id']
+   link = videosSearch.result()['result'][0]['link']
+   return id , link
+
+def ytsn(query):
+   videosSearch = VideosSearch(query, limit = 1)
+   #print(videosSearch.result()['result'])
+   id = videosSearch.result()['result'][0]['id']
+   link = videosSearch.result()['result'][0]['link']
+   title = videosSearch.result()['result'][0]['title']
+   img = videosSearch.result()['result'][0]['thumbnails'][0]['url']
+   return id , link , title , img 
+
+def dytdl(link):
+      ytdl = yt_dlp.YoutubeDL()
+      ytdl.download(link)
+      info=ytdl.extract_info(link)
+      id = info['id']
+      return id 
+
+def urlytdl(link):
+      ydl_opts = {'format': 'mp4/240/best',
+  }
+      ytdl = yt_dlp.YoutubeDL(ydl_opts)
+      info= ytdl.extract_info(link,download=False)
+      return info
+
+
+
+
+def ytdl(link):
+   ytlink = [ link ] 
+   ydl_opts = {
+    'format': 'mp3/bestaudio/best',
+    'postprocessors': [{  # Extract audio using ffmpeg
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        }]
+      }
+   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(ytlink)
+
+
+def pwrite(playlistlink):
+  playlist_id = playlistlink[34:].split('?')[0]
+  URL2 = "https://api.spotify.com/v1/playlists/"+ playlist_id +"?access_token=" + token  
+  r2 = requests.get(URL2)
+  pyname = r2.json()['name']
+  with open("playlist.csv","a+",newline='\n') as filec:
+           cwrite = csv.writer(filec)
+           cwrite.writerow([playlistlink,pyname, playlist_id])
+
+
+
+def pread():
+   global cread
+   filec = open("playlist.csv","r")
+   cread=csv.reader(filec)
+   return cread
+
+
+
+
+def getplay(playlistlink):
+     playlist_id = playlistlink[34:].split('?')[0]
+     URL = "https://api.spotify.com/v1/playlists/" + playlist_id  + "/tracks?access_token=" + token
+     URL2 = "https://api.spotify.com/v1/playlists/"+ playlist_id +"?access_token=" + token  
+     r2 = requests.get(URL2)
+     pyname = 'Playlist/' + r2.json()['name'] + "'"
+     r = requests.get(url = URL) 
+     total = r.json()['total']
+     count=0
+     y=False
+     while not y:  
+      items = r.json()['items']
+      for i in range(len(items)):
+         count +=1
+         #print(count,r.json()['items'][i]["track"]["external_urls"]["spotify"])
+         ytdl(ytsq(r.json()['items'][i]["track"]["external_urls"]["spotify"])[1])
+         for name in os.listdir():
+           if ytsq(r.json()['items'][i]["track"]["external_urls"]["spotify"])[0] in name:
+              rclone(name,pyname)
+
+      else:
+         if count ==total:
+            y = True
+            break
+         URL = r.json()['next'] +'&'+ "access_token=" + token
+         r = requests.get(url = URL)
+     return items
+
+
+
+
