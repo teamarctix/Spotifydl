dc289d1ea3f345ac654ff500d1cb1752d415f198
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-01 08:21:38 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,59 @@
+import os
+
+
+os.system("pip install psycopg2")
+
+import psycopg2
+
+conn = psycopg2.connect(database="emlvwsts",
+
+                        host="mahmud.db.elephantsql.com",
+
+                        user="emlvwsts",
+
+                        password="QZplqRjrPc5y3n2cf6VwgdvhcUZJ1hXV",
+
+                        port="5432")
+
+cursor = conn.cursor()
+
+def create_table():
+
+  
+
+  sql ='''CREATE TABLE Music(
+
+   URL VARCHAR(255) NOT NULL
+
+)'''
+
+  cursor.execute(sql)
+
+  print("Table created successfully........")
+
+  conn.commit()
+
+def insert_db(lnk):
+
+    query = "INSERT INTO REDLinks (url) VALUES('{}')".format(lnk)
+
+    cursor.execute(query)
+
+    conn.commit()
+
+def read_db():
+
+   cursor.execute("SELECT * FROM REDLinks")
+
+   data = cursor.fetchall()
+
+   return data
+
+def delall_db(name):
+
+    cursor.execute(f"DELETE FROM {name}")
+
+    conn.commit()
+
+#cursor.execute("DROP TABLE IF EXISTS PHLinks")
+
